f6490801acec4ea7ac6598449e5cdb211b64918d
takuto277 <takuto.72.soccer@gmail.com>
2024-10-13 03:57:56 +0900

    add

--- tca-tutorial/tca-tutorial/RepositoryListView.swift
+++ tca-tutorial/tca-tutorial/RepositoryListView.swift
@@ -12,5 +12,17 @@ import Foundation
 
 @Reducer
 public struct RepositoryList {
+    @ObservableState
+    public struct State: Equatable {
+        var repositories: [Repository] = []
+        var isLoading: Bool = false
+        
+        public init() {}
+    }
+    
+    public enum Action {
+        case onAppear
+    }
+    
     public init() {}
 }
