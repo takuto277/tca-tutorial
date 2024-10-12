da62ff2b19f79d6a1a6db1747ca3b5a154b2e76b
takuto277 <takuto.72.soccer@gmail.com>
2024-10-13 03:39:29 +0900

    add

--- tca-tutorial/tca-tutorial/RepositoryListView.swift
+++ tca-tutorial/tca-tutorial/RepositoryListView.swift
@@ -1,1 +1,16 @@
-https://github.com/pointfreeco/swift-composable-architecture
+//
+//  RepositoryListView.swift
+//  tca-tutorial
+//
+//  Created by 小野拓人 on 2024/10/13.
+//
+
+
+import ComposableArchitecture
+import Entity
+import Foundation
+
+@Reducer
+public struct RepositoryList {
+    public init() {}
+}
