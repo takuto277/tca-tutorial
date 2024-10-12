bea37db632e2b9c2f341efa8755c6276672b57b6
takuto277 <takuto.72.soccer@gmail.com>
2024-10-13 02:24:42 +0900

    create project

---
+++ tca-tutorial/tca-tutorial/tca_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_tutorialApp.swift
+//  tca-tutorial
+//
+//  Created by 小野拓人 on 2024/10/13.
+//
+
+import SwiftUI
+
+@main
+struct tca_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tca-tutorial/tca-tutorialTests/tca_tutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  tca_tutorialTests.swift
+//  tca-tutorialTests
+//
+//  Created by 小野拓人 on 2024/10/13.
+//
+
+import Testing
+@testable import tca_tutorial
+
+struct tca_tutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
